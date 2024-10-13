8a157cdd26886f1921e6527625d9ed2408648cf8
Eksaron <semencatenkos@gmail.com>
2024-10-13 20:30:05 +0300

    dz

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,17 @@
-// // import React from "react";
-// // import ReactDOM from "react-dom/client";
-// // import App from "./App.jsx";
-// // import "./index.css";
-
-// // ReactDOM.createRoot(document.getElementById("root")).render(
-// //   <React.StrictMode>
-// //     <App />
-// //   </React.StrictMode>
-// // );
-
-// // src/main.jsx
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./components/App";
-// // 1. Імпортуємо провайдер
-// import { Provider } from "react-redux";
-// // 2. Імпортуємо створений раніше стор
-// import { store } from "./redux/store";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-// src/main.jsx
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./components/App";
-// // 1. Імпортуємо провайдер
-// import { Provider } from "react-redux";
-// // 2. Імпортуємо створений раніше стор
-// import { store } from "./redux/store";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>{" "}
     </Provider>
   </React.StrictMode>
 );
